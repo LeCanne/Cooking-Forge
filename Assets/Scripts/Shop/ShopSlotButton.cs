e2ef7cbe9d6b7fd55f19c8b7d15d194fdb6f06e3
Le Canne <94301906+LeCanne@users.noreply.github.com>
2025-12-16 14:44:36 +0100

    ShopAdvancement

--- Assets/Scripts/Shop/ShopSlotButton.cs
+++ Assets/Scripts/Shop/ShopSlotButton.cs
@@ -1,6 +1,26 @@
+using TMPro;
 using UnityEngine;
 
 public class ShopSlotButton : MonoBehaviour
 {
-    
+    public ResourceData resourceGiven;
+    public int price;
+
+    public TMP_Text priceShown;
+   
+
+    public void Start()
+    {
+        priceShown.text = price.ToString();
+    }
+
+    public void BuyResource()
+    {
+        if (PlayerResourcesHandler.Instance.Buyable(price))
+        {
+            PlayerResourcesHandler.Instance.GiveResource(resourceGiven);
+        }
+    }
+
+
 }
